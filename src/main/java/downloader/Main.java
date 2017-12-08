1381b58e1705f0da6530840626c067782796e73d
AdityaHandadi <phadithya@gmail.com>
2017-12-08 02:49:48 -0600

    first commit

---
+++ src/main/java/downloader/Main.java
@@ -0,0 +1,9 @@
+package downloader;
+
+import java.io.IOException;
+
+public class Main {
+    public static void main(String args[]) throws IOException, InterruptedException {
+        DownloadImdbData.downloadFromS3();
+    }
+}
